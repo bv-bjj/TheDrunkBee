8500bc1e5ce06c3e6a4db16ff03b8b8f023c375a
Martín Madridejos <martindejos@gmail.com>
2021-04-30 07:57:17 +0200

    hola

---
+++ README.md
@@ -0,0 +1,54 @@
+# The drunk bee:beers:
+<img src=".\img\avispa-lostinchos.png" width="200" height="200">
+
+
+### Description
+
+The Drunk Bee is a multiplatform application (suitable for mobile phones, tablets and computers) designed by Los Tinchos that allows you to play a sacrifice selection system.
+
+The game is set on the topic of bees, so honey and beehives are all around, and, of course, the color yellow.
+
+1) We have a list of coders waiting a moment of glory. Moreover, any name can be added to the list through the button "Write a name".
+
+2) By pressing "Select random", one of the coders is randomly selected. 
+
+3) Afterwards, by clicking the button "drink beer" button, the selected coder is automatically out of the list, and consequently the beer gets emptied. And, of course, ¡the selected coder has to drink!
+
+4) Once the list is empty, you can click reset and restart the game. Enjoy!
+
+### Members of los Tinchos
+- Solomon Nweke (Scrum Master) 
+- Andrea Suárez 
+- Gabriela Baquerizo 
+- Martín Madridejos 
+- Franco Calviño 
+
+
+### Technologies
+<img src="https://img.icons8.com/color/48/000000/javascript.png"/><img src="https://img.icons8.com/color/48/000000/html-5--v1.png"/><img src="https://img.icons8.com/color/48/000000/css3.png"/><img src="https://img.icons8.com/fluent/48/000000/github.png"/>
+
+
+### Programmes
+- [Figma][webFigma]: Prototyping.
+- [Trello][webTrello]: Task table.
+- [Slack][webSlack]: information and comunication.
+- [Visual Studio Code][webVisual]: Code editor.
+- [Zoom][webZoom]: Comunication and share screen.
+
+### Demo
+
+### Elements to highlight and Problems
+- When we had the first sprint we felt that we did not advance much but after talking about it, we agreed and the second half we worked much better and we advanced while we learned.
+- It took us a lot to come up with a final prototype and we made a lot of changes along the way.
+- We wanted to add the styles with SCSS and we tried it at the beginning but since we didn't have much time at the end we decided to finish the project in CSS.
+- We are happy with our final project. We have learned a lot by helping each other and coding together.
+
+### technical description
+
+
+<!-- links -->
+[webFigma]: figma.com
+[webTrello]: trello.com
+[webSlack]: trello.com
+[webVisual]: https://visualstudio.microsoft.com/es/
+[webZoom]: zoom.com
---
+++ js/index.js
@@ -0,0 +1,147 @@
+import {listaCoders} from "./data.js"
+
+// Variables 
+
+let randomButton = document.getElementById("buttonRandom")
+let killButton = document.getElementById("buttonDrink")
+let randomName 
+let resetCoders = document.getElementById('buttonRestart')
+let rutaAbeja = document.getElementById('abeja')
+let audioAbeja = document.getElementById('sonidoAbeja')
+let usAbout = document.getElementById("sobreNosotros")
+let play = document.getElementById("comoJugar")
+let homePage = document.getElementById("principal")
+let main1 = document.getElementById("mainHome")
+let main2 = document.getElementById("mainAboutUs")
+let main3 = document.getElementById("mainPlay") 
+let beer1 = document.getElementById("cervezaLlena")
+let beer2 = document.getElementById("cervezaVacia")
+
+// Eventlistener
+
+randomButton.addEventListener('click', () => {
+    selectRandom() 
+    cambioImagen2()
+})
+
+killButton.addEventListener('click', () => {
+    killCoder();
+    cambioImagen()
+})
+
+resetCoders.addEventListener('click', ()=>{
+    location.reload()
+})
+
+rutaAbeja.addEventListener('click', ()=>{
+    audioAbeja.play()
+})
+
+usAbout.addEventListener('click', ()=>{
+    paginaAboutUs()
+})
+play.addEventListener('click', ()=>{
+    paginaHowToPlay()
+})
+homePage.addEventListener('click', ()=>{
+    paginaHome()
+})
+// Función: desplegar nombres
+
+function desplegarNombres() {
+    let templateHTML = ""
+    for(let i=0; i < listaCoders.length; i++) {
+    templateHTML += `<li>🍺${listaCoders[i].nombre}</li>` 
+    }
+    document.getElementById("listaNombres").innerHTML = templateHTML;
+}
+
+//  Función: selección random de nombres         
+
+function selectRandom() {
+    randomName = listaCoders [Math.floor(Math.random() * listaCoders.length)]
+    document.getElementById('printName').innerHTML = randomName.nombre;
+}
+
+// Función: quitar al nombre de la lista
+
+function killCoder() {
+    for(let i=0; i < listaCoders.length; i++) {
+        if (listaCoders [i] === randomName){
+            listaCoders.splice(i,1)
+        }
+    }
+    document.getElementById('printName').innerHTML = '';
+    desplegarNombres()
+}
+desplegarNombres()
+
+// Funciones cambio de página
+
+function paginaAboutUs() {
+    if (main1.style.display == "block") {
+        (main1.style.display = "none")
+    }
+    if (main2.style.display == "none") {
+        (main2.style.display = "block")
+    }
+    if (main3.style.display == "block") {
+        (main3.style.display = "none")
+    }
+}
+function paginaHowToPlay() {
+    if (main1.style.display == "block") {
+        (main1.style.display = "none")
+    }
+    if (main3.style.display == "none") {
+        (main3.style.display = "block")
+    }
+    if (main2.style.display == "block") {
+        (main2.style.display = "none")
+    }
+}
+function paginaHome() {
+    if (main2.style.display == "block") {
+        (main2.style.display = "none")
+    }
+    if (main1.style.display == "none") {
+        (main1.style.display = "block")
+    }
+    if (main3.style.display == "block") {
+        (main3.style.display = "none")
+    }
+}
+
+// funciones cambio página
+
+function cambioImagen(){
+    if (beer1.style.display == "block") {
+        (beer1.style.display = "none")
+    }
+    if (beer2.style.display == "none") {
+        (beer2.style.display = "block")
+    }
+}
+
+function cambioImagen2() {
+    if (beer2.style.display == "block") {
+        (beer2.style.display = "none")
+    }
+    if (beer1.style.display == "none") {
+        (beer1.style.display = "block")
+    }
+}
+
+// funcion añadir coder
+
+let añadirCoder = document.getElementById("botonCoder");
+añadirCoder.onclick = getInputValue
+
+function getInputValue() {
+    let inputValue = document.getElementById("addCoder").value
+    if( inputValue != ""){
+    listaCoders.push({nombre: inputValue})
+    desplegarNombres()
+}
+}
+

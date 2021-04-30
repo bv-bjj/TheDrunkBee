60e7cbaa76b67bb5ce72e0b4bdbe7903c83a776c
martindejos <78349147+martindejos@users.noreply.github.com>
2021-04-30 07:54:17 +0200

    commit
    s

---
+++ Abeja/abeja-borracha-main/js/data.js
@@ -0,0 +1,85 @@
+export const listaCoders = [
+    {
+        nombre: "David H. "
+    },
+    {
+        nombre: "Diego G. "
+    },
+    {
+        nombre: "Franco C. "
+    },
+    {
+        nombre: "Sergio C. "
+    },
+    {
+        nombre: "Alexandra G. "
+    },
+    {
+        nombre: "David S. "
+    },
+    {
+        nombre: "Diego C. "
+    },
+    {
+        nombre: "Sergi V. "
+    },
+    {
+        nombre: "Abdulha I. "
+    },
+    {
+        nombre: "Anya K. "
+    },
+    {
+        nombre: "Armando M. "
+    },
+    {
+        nombre: "Leah C. "
+    },
+    {
+        nombre: "Andrés P. "
+    },
+    {
+        nombre: "Andrea C. "
+    },
+    {
+        nombre: "Jose Miguel. "
+    },
+    {
+        nombre: "Amr H. "
+    },
+    {
+        nombre: "Jael M. "
+    },
+    {
+        nombre: "Thierno S. "
+    },
+    {
+        nombre: "Berta L. "
+    },
+    {
+        nombre: "Gabriela B. "
+    },
+    {
+        nombre: "Nweke S. "
+    },
+    {
+        nombre: "Cris M. "
+    },
+    {
+        nombre: "Snezhana S "
+    },
+    {
+        nombre: "Andrea S "
+    },
+    {
+        nombre: "Martín M "
+    },
+    {
+        nombre: "Joan R "
+    },
+    {
+        nombre: "Gabriela P "
+    },
+    
+        
+    ]
